9bc878c579a23fea30612b0eb05af0fa5a61ccc5
Hak-Seok Lee <fantasytotal@naver.com>
2024-10-01 16:42:05 +0900

    Delete EnjoyFood_Project directory

--- EnjoyFood_Project/src/main/java/user/controller/UserAPI.java
+++
@@ -1,9 +0,0 @@
-package user.controller;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet({ "/api/v1/user", "/api/v1/user/*" })
-public class UserAPI extends HttpServlet {
-
-}
