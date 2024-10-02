ef471446866eeae9d361c08f31e1dea76abaa2e2
JM <qwer123toy@gmail.com>
2024-10-02 17:56:19 +0900

    [feat] cafeReview 	- 리뷰 내용 추가

---
+++ src/main/java/main/controller/CafeReviewServlet.java
@@ -0,0 +1,17 @@
+package main.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/cafeReview")
+public class CafeReviewServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/view/cafeReview.jsp").forward(req, resp);
+	}
+}
