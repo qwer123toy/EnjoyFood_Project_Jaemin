f9ec8c0e051b7cbb585b0554b2f3e1444a12af44
AHHyeon12 <lleaa12@naver.com>
2024-10-04 15:40:42 +0900

    <fix> cafeteriaMapper select문 전체 ResultMap 추가로 수정함

--- src/main/java/main/controller/CafeDetailsServlet.java
+++ src/main/java/main/controller/CafeDetailsServlet.java
@@ -13,16 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 import Cafeteria.Cafeteria;
 import Cafeteria.CafeteriaService;
 import Cafeteria.CafeteriaServiceImple;
+import lombok.extern.slf4j.Slf4j;
 
 @WebServlet("/cafeteria")
+@Slf4j
 public class CafeDetailsServlet extends HttpServlet {
-	private CafeteriaService service = CafeteriaServiceImple.getInstance();
+	public CafeteriaService service = CafeteriaServiceImple.getInstance();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// cafeteria 선언 추가
 		 try {
-	            Cafeteria cafeteria = service.selectByName("카페 C");
+			 // 조회된 가게 정보를 cafeteria로 저장
+	            Cafeteria cafeteria = service.selectByName("꽃가람");
+	            log.info(cafeteria.toString());
 	            req.setAttribute("cafeteria", cafeteria);
+//	            JSP 페이지(cafeDetails.jsp)로 전달하여 렌더링
 	            req.getRequestDispatcher("/WEB-INF/view/cafeDetails.jsp").forward(req, resp);
 	        } catch (Exception e) {
 	            log("Error fetching cafeteria details", e);
